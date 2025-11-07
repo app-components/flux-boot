631ae48675864ab0729f6213b6a3e5f8ebc1cdbf
Adib Saikali <asaikali@gmail.com>
2025-11-07 06:39:46 -0500

    feat: keep all old releases

--- 2.7/build.sh
+++ 2.7/build.sh
@@ -9,10 +9,13 @@ VERSION=$(yq '.directories[] | select(.path == "upstream") | .contents[] | selec
 echo "Building flux-boot version $VERSION"
 
 FLUX_YAML="latest/gotk-components.yaml"
+FLUX_YAML_VERSION="releases/gotk-components-${VERSION}.yaml"
 
 # Build the bootstrap manifest
 echo "Running kustomize build..."
 kustomize build . > ${FLUX_YAML}
+kustomize build . > ${FLUX_YAML_VERSION}
 
 echo "Generated files:"
 echo "  - ${FLUX_YAML}"
+echo "  - ${FLUX_YAML_VERSION}"
