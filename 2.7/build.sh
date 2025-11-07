b0a36d73809797a2b8daaff7ed31379f30133977
Adib Saikali <asaikali@gmail.com>
2025-11-07 06:28:20 -0500

    feat: make the build.sh script extract version number from vendir

--- 2.7/build.sh
+++ 2.7/build.sh
@@ -3,7 +3,10 @@ set -e
 
 vendir sync
 
-echo ""
+
+# Read version from VERSION file
+VERSION=$(yq '.directories[] | select(.path == "upstream") | .contents[] | select(.path == "manifests") | .githubRelease.tag' vendir.lock.yml)
+echo "Building flux-boot version $VERSION"
 
 FLUX_YAML="release/gotk-components.yaml"
 
